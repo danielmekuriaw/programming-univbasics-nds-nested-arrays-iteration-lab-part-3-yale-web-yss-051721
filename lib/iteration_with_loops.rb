e3e3190a4b7f0c38bddd804cb15c7369d9cfd2de
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 00:59:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
   
     while inner < src[outer].length do
       
-      if src[outer][inner].type == "string".type
+      if type(src[outer][inner]) == type("string")
         conc += src[outer][inner]
       end
       
