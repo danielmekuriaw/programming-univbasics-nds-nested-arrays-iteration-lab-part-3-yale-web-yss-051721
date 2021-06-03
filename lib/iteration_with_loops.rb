0bf197dd04ec2baece2cb0997346b174da89318e
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 00:58:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,7 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   conc = ""
+  outer = 0
   while outer < src.length do
     inner = 0
   
